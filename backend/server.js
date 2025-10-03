24ff48354af22bad43275a9c3fa4a94e83e4ccb7
Nitesh Mishra <mishranitesh45176@gmail.com>
2025-10-03 23:25:11 +0530

    secondchange

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import { connectDB } from './configs/db.js';
-import slotRoutes from './routes/slot.Routes.js'; // adjust path if needed
+import slotRoutes from '../backend/routes/slot.Routes.js'; // adjust path if needed
 
 dotenv.config();
 
